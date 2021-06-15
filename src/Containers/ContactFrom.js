59a9178c315dc39b50df75ef8c394ca1dccbdd62
imran mirza <imranmirza070901@gmail.com>
2021-06-15 20:24:50 +0530

    Added footer and contact form containers without style

---
+++ src/Containers/ContactFrom.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ContactFormComponent from "../Components/ContactFormComponent";
+
+class ContactForm extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<ContactFormComponent />
+			</React.Fragment>
+		);
+	}
+}
+
+export default ContactForm;
---
+++ src/Containers/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+import FooterComponent from "../Components/footerComponent";
+
+class Footer extends Component{
+	render() {
+		return (
+			<React.Fragment>
+					<FooterComponent />
+			</React.Fragment>
+		);
+	}
+}
+
+export default Footer;
