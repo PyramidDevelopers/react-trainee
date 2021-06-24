d0f196a3bd8eb9bc3fba4e3cda2b66693820af98
Hilag Shah <64307441+shahhilag@users.noreply.github.com>
2021-06-25 01:23:40 +0530

    Update Who.js

--- src/MyComponents/Who/Who.js
+++ src/MyComponents/Who/Who.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import './Who.css'
 
@@ -9,16 +8,3 @@ const Who = () => {
 }
 
 export default Who
-||||||| 7fd4f19
-=======
-import React from 'react'
-import './Who.css'
-
-const Who = () => {
-    return (
-        <div></div>
-    )
-}
-
-export default Who
->>>>>>> f91342f34b56211a918ef222da226cdc6d75c16d
