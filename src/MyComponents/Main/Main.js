2ad90f706522d27457ab8a10002d81f53f0b5a76
Hilag Shah <64307441+shahhilag@users.noreply.github.com>
2021-06-25 01:19:32 +0530

    Update Main.js

--- src/MyComponents/Main/Main.js
+++ src/MyComponents/Main/Main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import './Main.css'
 
@@ -9,16 +8,3 @@ const Main = () => {
 }
 
 export default Main
-||||||| 7fd4f19
-=======
-import React from 'react'
-import './Main.css'
-
-const Main = () => {
-    return (
-        <div></div>
-    )
-}
-
-export default Main
->>>>>>> f91342f34b56211a918ef222da226cdc6d75c16d
