920ce50b1e8e210357873b7f265024ba1d8fba9e
Raashi Agarwal <raashiaggarwal21@gmail.com>
2021-06-27 01:11:30 +0530

    Added Why section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { OurTeam } from './components/our team/OurTeam';
 import { Who } from './components/who/Who'
 import { What } from './components/what/What';
+import { Why } from './components/why/Why';
 
 function App() {
   return (
     <div className="main-app">
       <Who />
       <What />
+      <Why />
       <OurTeam />
     </div>
   );
---
+++ src/components/why/Why.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { InfoText } from '../text/InfoText'
+import leftImage from '../../assets/choose-left.png'
+import rightImage from '../../assets/choose-right.png'
+import './Why.css'
+
+export const Why = () => {
+    let question = "WHY CHOOSE US"
+    let paragraph1 = "Our main focus is quality. We will give you the best looking, most user friendly products with a firm code base, for the right price."
+    let paragraph2 = "If you partner with Pyramid, We will ensure that you receive an end product that exceeds expectations. We follow Agile Development practices to ensure we fulfil our promises."
+
+    return (
+        <div className="why-section">
+            <InfoText question={question} paragraph1={paragraph1} paragraph2={paragraph2} />
+            <div className="why-left-image-container">
+                <img src={leftImage} className="why-left-image" alt="" />
+                <div className="why-horizontal-rect" />
+            </div>
+            <div className="why-right-image-container">
+                <div className="why-horizontal-rect" />
+                <img src={rightImage} className="why-right-image" alt="" />
+            </div>
+            <div className="why-vertical-rects-container">
+                <div className="why-vertical-rect" />
+                <div className="why-vertical-rect" />
+                <div className="why-vertical-rect-dark" />
+                <div className="why-vertical-rect" />
+            </div>
+            <div className="why-vertical-rects-container-right">
+                <div className="why-vertical-rect-dark" />
+                <div className="why-vertical-rect" />
+            </div>
+        </div>
+    )
+}
